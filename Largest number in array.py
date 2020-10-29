7ef77603b19a03b7f28b038e7bdfa9b73b2cc3ad
Ayobami <68803580+ayobamiakomolafe@users.noreply.github.com>
2020-10-29 12:17:49 -0700

    Programe to print Largest number in array
    Programe to print Largest number in array

---
+++ Largest number in array.py
@@ -0,0 +1,24 @@
+print'Continue Entering number for the array, when all numbers in array are entered type-in done '
+array=list()
+
+while True:
+    num=raw_input('Enter Number:')
+    try:
+        if num == 'done':
+            break
+        else:
+            array.append(int(num))
+            
+    except:
+        print('\nPlease make Sure a Number is Entered, When you are done entering desired number in array\ntype-in done \n')
+        continue
+
+largest=(max(array))
+
+print('\n')
+print('The largest number in the array is '+ `largest`)
+
+
+
+    
+    
