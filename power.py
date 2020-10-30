5b25b8ff8019539c54f357031715e26dda41b09d
Ayobami <68803580+ayobamiakomolafe@users.noreply.github.com>
2020-10-30 07:32:31 -0700

    Programe to calculate power
    Programe to calculate power

--- power.py
+++ power.py
@@ -1,4 +1,20 @@
-base = int(input("Enter your base number: "))
-power = int(input("Enter your power/exponent: "))
+Ok='No'
+while Ok=='No':
+    num=raw_input('Enter Number: ')
+    power=raw_input('To What Power:')
+    try:
+        num=int(num)
+        power=int(power)
+        Ok='yes'
+    except:
+        print('Make sure a Number has been Entered ')
 
-print(base**power)
+Answer=num
+if power == 0:
+    Answer=1
+else:
+    for i in range(power-1):
+        Answer=Answer*num
+ 
+print `num`+ '^' + `power` + '=' + `Answer`        
+
